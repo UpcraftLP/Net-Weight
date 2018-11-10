0379701aa711e179d8370154bfa595b590fe619c
Joshua Schirra <j-schirra@gmx.de>
2018-11-10 17:04:08 +0100

    added toast for new top weight

---
+++ src/main/java/com/github/upcraftlp/netweight/client/HeavyFishToast.java
@@ -0,0 +1,52 @@
+package com.github.upcraftlp.netweight.client;
+
+import com.github.upcraftlp.netweight.FishStats;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.toasts.*;
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.resources.I18n;
+
+public class HeavyFishToast implements IToast {
+
+    private long firstDrawTime;
+    private boolean newDisplay;
+    private final String title = I18n.format("toast.net_weight.newTopWeight");
+    private String weightString;
+
+    public HeavyFishToast(int weight) {
+        this.weightString = FishStats.Type.WEIGHT.format(weight);
+    }
+
+    public static void addOrUpdate(int weight) {
+        GuiToast toastGui = Minecraft.getMinecraft().getToastGui();
+        HeavyFishToast toast = toastGui.getToast(HeavyFishToast.class, IToast.NO_TOKEN);
+        if(toast != null) {
+            toast.updateWeight(weight);
+        }
+        else {
+            toastGui.add(new HeavyFishToast(weight));
+        }
+    }
+
+    private void updateWeight(int weight) {
+        this.weightString = FishStats.Type.WEIGHT.format(weight);
+        this.newDisplay = true;
+    }
+
+    @Override
+    public Visibility draw(GuiToast toastGui, long delta) {
+        if(this.newDisplay) {
+            this.firstDrawTime = delta;
+            this.newDisplay = false;
+        }
+
+        toastGui.getMinecraft().getTextureManager().bindTexture(TEXTURE_TOASTS);
+        GlStateManager.color(1.0F, 1.0F, 1.0F);
+        toastGui.drawTexturedModalRect(0, 0, 0, 64, 160, 32);
+
+        toastGui.getMinecraft().fontRenderer.drawString(this.title, 18, 7, -256);
+        toastGui.getMinecraft().fontRenderer.drawString(this.weightString, 18, 18, -1);
+
+        return delta - this.firstDrawTime < 5000L ? IToast.Visibility.SHOW : IToast.Visibility.HIDE;
+    }
+}
--- src/main/java/com/github/upcraftlp/netweight/handler/FishHandler.java
+++ src/main/java/com/github/upcraftlp/netweight/handler/FishHandler.java
@@ -1,9 +1,10 @@
 package com.github.upcraftlp.netweight.handler;
 
 import com.github.upcraftlp.netweight.*;
+import com.github.upcraftlp.netweight.client.HeavyFishToast;
 import com.github.upcraftlp.netweight.config.NetWeightConfig;
 import net.minecraft.entity.player.EntityPlayerMP;
-import net.minecraft.init.MobEffects;
+import net.minecraft.init.*;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.potion.PotionEffect;
 import net.minecraftforge.event.entity.player.ItemFishedEvent;
@@ -29,8 +30,12 @@ public class FishHandler {
                     if(luck != null) multiplier += luck.getAmplifier() * 0.5D;
                     int weight = (int) ((Math.abs(event.getEntityPlayer().getRNG().nextGaussian() * multiplier) * 1000) + 35);
                     stack.getTagCompound().setInteger(KEY_FISH_WEIGHT, weight);
-                    int previous = ((EntityPlayerMP) event.getEntityPlayer()).getStatFile().readStat(FishStats.HEAVIEST_FISH);
-                    if(previous < weight) event.getEntityPlayer().addStat(FishStats.HEAVIEST_FISH, weight - previous);
+                    int previous = ((EntityPlayerMP) event.getEntityPlayer()).getStatFile().readStat(FishStats.HEAVIEST_FISH_CAUGHT);
+                    if(previous < weight) {
+                        event.getEntityPlayer().addStat(FishStats.HEAVIEST_FISH_CAUGHT, weight - previous);
+                        HeavyFishToast.addOrUpdate(weight);
+                        event.getEntityPlayer().playSound(SoundEvents.ENTITY_EXPERIENCE_ORB_PICKUP, 1.0F, event.getEntityPlayer().getRNG().nextFloat() * 0.6F + 0.4F);
+                    }
                     event.getEntityPlayer().addStat(FishStats.TOTAL_FISH_WEIGHT, weight);
                 }
             }
