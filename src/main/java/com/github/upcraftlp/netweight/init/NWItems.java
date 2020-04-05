14f804063f2bc8376a724e099e93783fcb7151d4
UpcraftLP <j-schirra@gmx.de>
2020-04-05 14:29:11 +0200

    make items be food

--- src/main/java/com/github/upcraftlp/netweight/init/NWItems.java
+++ src/main/java/com/github/upcraftlp/netweight/init/NWItems.java
@@ -4,11 +4,13 @@ import com.github.glasspane.mesh.api.annotation.AutoRegistry;
 import com.github.glasspane.mesh.api.registry.AutoRegistryHook;
 import com.github.upcraftlp.netweight.NetWeight;
 import com.github.upcraftlp.netweight.item.IngredientItem;
+import net.minecraft.item.FoodComponent;
 import net.minecraft.item.Item;
 
 @AutoRegistry(value = Item.class, modid = NetWeight.MODID, registry = "item")
 public class NWItems implements AutoRegistryHook {
 
-    public static final Item TROUT = new IngredientItem();
-    public static final Item EEL = new IngredientItem();
+    public static final Item TROUT = new IngredientItem(new Item.Settings().food(new FoodComponent.Builder().hunger(2).saturationModifier(0.1F).build()));
+    public static final Item EEL = new IngredientItem(new Item.Settings().food(new FoodComponent.Builder().hunger(2).saturationModifier(0.1F).build()));
+
 }
